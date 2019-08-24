9f338577df69d3712aa6a28093e7e8c70f7193c5
snoopydoggie <53892985+snoopydoggie@users.noreply.github.com>
2019-08-24 10:33:16 +0530

    Add files via upload

---
+++ 2b_week2_bubble_sort.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,k;
+	cout<<"Enter n of array[n] bsort: ";
+	cin>>n;
+
+	int arr[n];
+
+	cout<<"\nEnter array elements :";
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+
+int i, j;  
+    for (i = 0; i < n-1; i++) {   
+      
+    // Last i elements are already in place  
+    for (j = 0; j < n-i-1; j++)  
+        if (arr[j] > arr[j+1]){
+			int temp=arr[j];
+			arr[j]=arr[j+1];
+			arr[j+1]=temp;
+		}
+	}
+	
+	for(int i=0;i<n;i++)
+		cout<<"\n"<<arr[i];
+
+	cout<<"Enter k:";
+	cin>>k;
+	
+	for(int i=n-1;i>n-k-1;i--)
+		cout<<"\t"<<arr[i];
+}
